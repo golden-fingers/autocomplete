47c22f23b7d5eb8d0a65684d4e7cd66d4f02c5a6
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-02-21 05:01:32 -0500

    "adding result to file"

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
+var result = fs.readFileSync(__dirname,'/form.html',utf8);
 http.createServer(function(req,res){
-   fs.readFileSync(__dirname,'/form.html',utf8);
-   res.end(fs);
+
+   res.end(result);
  }).listen(process.env.PORT || 8080,function(){
    console.log('Listening on 8080');
  });
