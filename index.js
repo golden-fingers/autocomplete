dcb0398f961673cebd025e579cecd58302498e7c
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-02-21 04:47:40 -0500

    "res.end(file)"

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function(req,res){
    fs.readFileSync(__dirname,'/form.html',utf8);
+   res.end(fs);
  }).listen(8080,function(){
    console.log('Listening on 8080');
  });
